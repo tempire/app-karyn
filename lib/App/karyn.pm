1a0980530bc4a7dae67dd83c51d4ac31e23cdfac
Glen Hinkle <glen@empireenterprises.com>
2011-02-08 00:09:12 -0500

    read RIAK_HOST env

--- lib/App/karyn.pm
+++ lib/App/karyn.pm
@@ -8,7 +8,7 @@ use Net::Riak;
 use lib '/Users/glen/Projects/riak-tiny/Riak-Tiny/lib';
 use Riak::Tiny;
 
-has host => 'http://127.0.0.1:8098';
+has host => $ENV{RIAK_HOST} || 'http://127.0.0.1:8098';
 has riak => sub { Net::Riak->new(host => shift->host) };
 has tiny => sub { Riak::Tiny->new(host => shift->host) };
 has client => sub { Mojo::Client->new };
